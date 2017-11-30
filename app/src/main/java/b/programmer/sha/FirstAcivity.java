41a10795bbe14980b8b93e68adfa77c50cb89124
ar-programmer <alexander.rabau@gmail.com>
2017-12-01 00:16:48 +0100

    commit8

--- app/src/main/java/b/programmer/sha/FirstAcivity.java
+++ app/src/main/java/b/programmer/sha/FirstAcivity.java
@@ -4,9 +4,22 @@ import android.app.Activity;
 import android.app.AlarmManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
 
 /**
  * Created by alexanderrabau on 19.11.17.
@@ -54,9 +67,14 @@ public class FirstAcivity extends Activity {
 
     }
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final String imageFilePath = Environment.getExternalStorageDirectory().getPath() + "/picture.jpg";
+
     public void onAddNoteClick(View view) {
 
-        SharedPreferences sharedPref = getSharedPreferences("sharePref", MODE_PRIVATE);
+
+
+   /*     SharedPreferences sharedPref = getSharedPreferences("sharePref", MODE_PRIVATE);
      int   i = sharedPref.getInt("IAddText", 0);  // get stored int Value
 
         i++;                                   // add one to the stored Value
@@ -64,8 +82,40 @@ public class FirstAcivity extends Activity {
         Intent j = new Intent(this, AddNoteActivity.class);  // Warum funktioniert hier nicht nur ledglich THIS ANZUGEBEN?
         //       j.putExtra("output", newValue);
 
-        startActivity(j);
+        startActivity(j);  */
 
+
+        // TODO Auto-generated method stub
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 0);
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            Uri targetUri = data.getData();
+          //    textTargetUri.setText(targetUri.toString());
+            Bitmap bitmap;
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+            //    Resources res = getResources(bitmap);
+
+                RelativeLayout bg = (RelativeLayout) findViewById(R.id.relativelayout);
+                Drawable drawable = new BitmapDrawable(getResources(), bitmap);
+                bg.setBackgroundDrawable(drawable);
+           //     RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativelayout);
+           //     relativeLayout.setBackgroundDrawable(bitmap);
+
+          //                targetImage.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
 }
